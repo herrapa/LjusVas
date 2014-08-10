15a462c8e5dd4040d148fdf2b7cf555c9c24c2d5
herrapa <joakimsson@gmail.com>
2014-08-10 17:45:58 +0200

    Initial commit

---
+++ LjusVas.ino
@@ -0,0 +1,47 @@
+//Extremely simple program to work with a vase with RGB LEDS in it
+
+// the pins that the LED is attached to
+#define red 10
+#define green 11
+#define blue 13
+
+#define redColor 255
+#define greenColor 255
+#define blueColor 50
+
+void setup()  { 
+  pinMode(red, OUTPUT);
+  pinMode(green, OUTPUT);
+  pinMode(blue, OUTPUT);
+  
+  //loop through all colors for an effect
+  loopColorWithDelay(red, 10);
+  loopColorWithDelay(green, 10);
+  loopColorWithDelay(blue, 10);
+  
+  //set the final color
+  analogWrite(red, redColor);
+  analogWrite(green, greenColor);
+  analogWrite(blue, blueColor);
+} 
+
+void loopColorWithDelay(int color, int delayTime) {
+    analogWrite(red, 0);
+  analogWrite(green, 0);
+  analogWrite(blue, 0);
+  for (int i = 0; i <= 255; i++)
+  {
+    analogWrite(color, i);
+    delay(delayTime);
+  }
+  for (int i = 255; i >= 0; i--)
+  {
+    analogWrite(color, i);
+    delay(delayTime);
+  }
+}
+
+void loop()  
+{                           
+}
+
